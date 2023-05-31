a9aa4597ab4e133e15887901fa0441f01f4624a4
Felipe A Marques (Stardust) <Lipemarxgyn@gmail.com>
2023-05-31 11:28:25 -0300

    terminei

---
+++ trybe-exercicios/01 - Fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos/Exercicio4.js
@@ -0,0 +1,12 @@
+let ang1 = 60
+let ang2 = 60
+let ang3 = 60
+let eUmTriangulo
+
+if(ang1 + ang2 + ang3 == 180){
+ eUmTriangulo = 'É um Triangulo!'
+}
+else{
+eUmTriangulo = 'Não é um triangulo ;-;'
+}
+console.log(eUmTriangulo)
