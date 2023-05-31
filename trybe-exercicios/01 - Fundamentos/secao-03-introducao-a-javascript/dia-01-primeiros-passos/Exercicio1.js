01184206ad57ad9681653cdc838203e88cc21b08
Felipe A Marques (Stardust) <Lipemarxgyn@gmail.com>
2023-05-31 10:47:47 -0300

    adicionei os console logs

--- trybe-exercicios/01 - Fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos/Exercicio1.js
+++ trybe-exercicios/01 - Fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos/Exercicio1.js
@@ -1,8 +1,13 @@
 let num1 = 45
 let num2 = 56
 
-let adicao = num1 + num1
-let subtracao = num1 - num1
-let multiplicacao = num1 * num1
-let divisão = num1 / num1
-let modulo = num1 % num1
+let adicao = num1 + num2
+let subtracao = num1 - num2
+let multiplicacao = num1 * num2
+let divisão = num1 / num2
+let modulo = num1 % num2
+console.log(adicao)
+console.log(subtracao)
+console.log(multiplicacao)
+console.log(divisão)
+console.log(modulo)
