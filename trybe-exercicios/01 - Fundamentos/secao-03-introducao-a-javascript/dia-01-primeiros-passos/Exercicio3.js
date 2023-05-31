21ea426b3a1be143f488e1ee10b1c7fcd7a10d89
Felipe A Marques (Stardust) <Lipemarxgyn@gmail.com>
2023-05-31 11:13:15 -0300

    terminei

--- trybe-exercicios/01 - Fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos/Exercicio3.js
+++ trybe-exercicios/01 - Fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos/Exercicio3.js
@@ -1,8 +1,13 @@
-let num1 = 12
-let num2 = 13
+let num1 = 47
+let num2 = 65
+let num3 = 35
 
-if(num1 > num2){
- console.log(num1)
-} else{
-console.log(num2)
+if(num1 > num2 && num1 > num2){
+    console.log(num1 + ' é o maior numero')
 }
+if(num2 > num1 && num2 > num3){
+    console.log(num2 + ' é o maior numero')
+   }
+if(num3 > num1 && num3 > num2){
+    console.log(num3 + ' é o maior numero')
+   }
