beeedcb35acb1b9d3f0c679898280ba9105f5db0
Felipe A Marques (Stardust) <Lipemarxgyn@gmail.com>
2023-06-01 20:56:35 -0300

    fiz o calculo para o valor do inss

--- trybe-exercicios/01 - Fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos/Exercicio7.js
+++ trybe-exercicios/01 - Fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos/Exercicio7.js
@@ -4,5 +4,15 @@ let inss = null;
 let salarioLiquido = null;
 
 if(salarioBruto < 1556.94){
-    inss = salarioBruto * 0,08 
+    inss = salarioBruto * 0.08 
 }
+if(salarioBruto >= 1556.95){
+    inss = salarioBruto * 0.09
+}
+if(salarioBruto >= 2594.93){
+    inss = salarioBruto * 0.11
+}
+if(salarioBruto > 5189.82){
+    inss = 570.88
+}
+console.log(inss)
