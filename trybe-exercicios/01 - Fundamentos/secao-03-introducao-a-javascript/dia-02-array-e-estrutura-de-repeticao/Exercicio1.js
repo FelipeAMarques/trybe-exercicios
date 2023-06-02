859f96de078a349ec570659ba61d719c4d5cde16
Felipe A Marques (Stardust) <Lipemarxgyn@gmail.com>
2023-06-02 09:56:58 -0300

    arrumei o segund requisito

--- trybe-exercicios/01 - Fundamentos/secao-03-introducao-a-javascript/dia-02-array-e-estrutura-de-repeticao/Exercicio1.js
+++ trybe-exercicios/01 - Fundamentos/secao-03-introducao-a-javascript/dia-02-array-e-estrutura-de-repeticao/Exercicio1.js
@@ -1,6 +1,6 @@
 const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]; 
 let somaNumbers = null;
-let mediaAritica = null;
+let mediaAritimetica = null;
 let mediaMoreThan20 = null;
 for(let index = 0; index < numbers.length; index += 1) {
     console.log(`${index} - ${numbers[index]}`)}
@@ -10,9 +10,10 @@ for(let index2 =0; index2 < numbers.length; index2 += 1) {
 }
 console.log(somaNumbers)
 // media aritimetica abaixo 
-mediaAritica = somaNumbers / 10;
+mediaAritimetica = somaNumbers / numbers.length
+
 // Se o resultado é maior que 20
-if(mediaAritica > 20){
+if(mediaAritimetica > 20){
 mediaMoreThan20 = 'O valor da média aritmética é maior que 20'
 } else{
     mediaMoreThan20 = 'O valor da média aritmética é menor ou igual a 20'
