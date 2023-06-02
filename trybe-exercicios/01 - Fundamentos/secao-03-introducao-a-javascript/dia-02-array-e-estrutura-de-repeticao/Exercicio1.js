d830d5be29d3283ec7b27fd659b221a6bc1a8373
Felipe A Marques (Stardust) <Lipemarxgyn@gmail.com>
2023-06-02 11:07:19 -0300

    deixei o cod melhor UwU

--- trybe-exercicios/01 - Fundamentos/secao-03-introducao-a-javascript/dia-02-array-e-estrutura-de-repeticao/Exercicio1.js
+++ trybe-exercicios/01 - Fundamentos/secao-03-introducao-a-javascript/dia-02-array-e-estrutura-de-repeticao/Exercicio1.js
@@ -3,27 +3,44 @@ let somaNumbers = null;
 let mediaAritimetica = null;
 let mediaMoreThan20 = null;
 let maiorValor = null;
+let impares = [];
+
 for(let index = 0; index < numbers.length; index += 1) {
     console.log(`${index} - ${numbers[index]}`)}
-// somei os numero da array acima ^
-for(let index2 =0; index2 < numbers.length; index2 += 1) {
-    somaNumbers = somaNumbers + numbers[index2]
+
+    // somei os numero da array acima ^
+
+for(let index =0; index < numbers.length; index += 1) {
+    somaNumbers = somaNumbers + numbers[index]
 }
 console.log(somaNumbers)
+
 // media aritimetica abaixo 
+
 mediaAritimetica = somaNumbers / numbers.length
 
 // Se o resultado é maior que 20
+
 if(mediaAritimetica > 20){
 mediaMoreThan20 = 'O valor da média aritmética é maior que 20'
 } else{
     mediaMoreThan20 = 'O valor da média aritmética é menor ou igual a 20'
 };
 console.log(mediaMoreThan20);
+
 // for para descobrir o maior valor
-for(index3 = 0; index3 < numbers.length; index3 += 1){
-    if(numbers[index3] > maiorValor) {
-       maiorValor = numbers[index3]
+
+for(let index = 0; index < numbers.length; index += 1){
+    if(numbers[index] > maiorValor) {
+       maiorValor = numbers[index]
     }
 }
 console.log(maiorValor)
+// Os numero impares do array 
+
+for(let index = 0; index < numbers.length; index += 1){
+    if(numbers[index] % 2 !=  0){
+        impares.push(numbers[index])
+    } 
+}
+console.log(impares)
