3785a6c186694820e00a4161cdeeeccd698d988f
Felipe A Marques (Stardust) <Lipemarxgyn@gmail.com>
2023-06-02 09:53:38 -0300

    arrumei o 2

--- trybe-exercicios/01 - Fundamentos/secao-03-introducao-a-javascript/dia-02-array-e-estrutura-de-repeticao/Exercicio1.js
+++ trybe-exercicios/01 - Fundamentos/secao-03-introducao-a-javascript/dia-02-array-e-estrutura-de-repeticao/Exercicio1.js
@@ -1,10 +1,14 @@
 const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]; 
-let somaNumbers;
-let mediaAritica
-let mediaMoreThan20
-// somei os numero da array acima ^
+let somaNumbers = null;
+let mediaAritica = null;
+let mediaMoreThan20 = null;
 for(let index = 0; index < numbers.length; index += 1) {
     console.log(`${index} - ${numbers[index]}`)}
+// somei os numero da array acima ^
+for(let index2 =0; index2 < numbers.length; index2 += 1) {
+    somaNumbers = somaNumbers + numbers[index2]
+}
+console.log(somaNumbers)
 // media aritimetica abaixo 
 mediaAritica = somaNumbers / 10;
 // Se o resultado é maior que 20
@@ -15,6 +19,4 @@ mediaMoreThan20 = 'O valor da média aritmética é maior que 20'
 };
 console.log(mediaMoreThan20);
 // for para descobrir o maior valor
-for(let index = 0; index < numbers.length; index += 1) {
-    console.log(`${index} - ${numbers[index]}`)
-}
+
