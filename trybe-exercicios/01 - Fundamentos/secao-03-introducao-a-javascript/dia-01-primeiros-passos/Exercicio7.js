91373f00ce0b3711e91d2ec634e61a7885293929
Felipe A Marques (Stardust) <Lipemarxgyn@gmail.com>
2023-06-01 21:44:14 -0300

    fiz certinho e terimei

--- trybe-exercicios/01 - Fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos/Exercicio7.js
+++ trybe-exercicios/01 - Fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos/Exercicio7.js
@@ -1,8 +1,8 @@
-let salarioBruto = 3000;
+let salarioBruto = 5000;
 let ir = null;
 let inss = null;
 let salarioLiquido = null;
-
+// calculando inss
 if(salarioBruto < 1556.94){
     inss = salarioBruto * 0.08 
 }
@@ -15,4 +15,24 @@ if(salarioBruto >= 2594.93){
 if(salarioBruto > 5189.82){
     inss = 570.88
 }
-console.log(inss)
+  salarioBruto = salarioBruto - inss 
+
+if(salarioBruto < 1903.98){
+    ir = 0 - 0
+}
+if(salarioBruto > 1903.99){
+    ir = salarioBruto * 0.075 - 142.80  
+}
+if(salarioBruto > 2826.66){
+    ir = salarioBruto * 0.15 - 354.80
+}
+if(salarioBruto > 3751.06){
+    ir = salarioBruto * 0.225 - 636.13 
+}
+if(salarioBruto > 4664.68){
+    ir = salarioBruto * 0.275 - 869.36
+}
+
+salarioLiquido = salarioBruto - ir 
+
+console.log(salarioLiquido)
