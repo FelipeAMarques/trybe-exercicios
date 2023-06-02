20c4726d964b32f437710b7aef8b0b3baf3c4841
Felipe A Marques (Stardust) <Lipemarxgyn@gmail.com>
2023-06-01 22:38:38 -0300

    fiz o 3 requisito

--- trybe-exercicios/01 - Fundamentos/secao-03-introducao-a-javascript/dia-02-array-e-estrutura-de-repeticao/Exercicio1.js
+++ trybe-exercicios/01 - Fundamentos/secao-03-introducao-a-javascript/dia-02-array-e-estrutura-de-repeticao/Exercicio1.js
@@ -1,5 +1,8 @@
 const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]; 
 let somaNumbers;
+let mediaAritica
 // somei os numero da array acima ^
 somaNumbers = numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6] + numbers[7] + numbers[8] + numbers[9];
-// media aritimetica 
+// media aritimetica abaixo 
+mediaAritica = somaNumbers / 10;
+
