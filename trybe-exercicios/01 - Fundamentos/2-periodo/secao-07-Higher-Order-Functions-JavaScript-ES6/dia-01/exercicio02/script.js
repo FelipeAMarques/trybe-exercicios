51135a26754c667229909eb2d883d7e88123df07
Felipe A Marques (Stardust) <Lipemarxgyn@gmail.com>
2023-07-10 21:35:32 -0300

    fiz o quinto requisito

---
+++ trybe-exercicios/01 - Fundamentos/2-periodo/secao-07-Higher-Order-Functions-JavaScript-ES6/dia-01/exercicio02/script.js
@@ -0,0 +1,7 @@
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+// Adicione seu código aqui || Utilize o find para encontrar o primeiro nome com cinco letras, caso ele exista:
+
+let nomes = names.find((name) => name.length === 5)
+
+console.log(nomes);
